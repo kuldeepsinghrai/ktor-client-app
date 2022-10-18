cc1b2c75eef36b8511ead79ec6d34cd4b17bfde8
Kuldeep Singh Rai <imkuldeepsinghrai@gmail.com>
2022-10-18 18:09:38 +0530

    Initial Commit

---
+++ app/src/main/java/com/kuldeepsinghrai/ktorclientapp/data/remote/PostsService.kt
@@ -0,0 +1,31 @@
+package com.kuldeepsinghrai.ktorclientapp.data.remote
+
+import com.kuldeepsinghrai.ktorclientapp.data.remote.dto.PostRequest
+import com.kuldeepsinghrai.ktorclientapp.data.remote.dto.PostResponse
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+
+interface PostsService {
+
+    suspend fun getPosts(): List<PostResponse>
+
+    suspend fun createPost(postRequest: PostRequest): PostResponse?
+
+    companion object {
+        fun create(): PostsService {
+            return PostsServiceImpl(
+                client = HttpClient(Android) {
+                    install(Logging) {
+                        level = LogLevel.ALL
+                    }
+                    install(JsonFeature) {
+                        serializer = KotlinxSerializer()
+                    }
+                }
+            )
+        }
+    }
+}
